678956de5ddd2d27ed0ece78e959465229ac7378
uriel.martinez <uriel.martinez@inateltech.com>
2025-07-07 01:40:11 -0600

    ultimos cambios p

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
-import { AuthGuard } from './auth/guards/auth.guard';
+import { authGuard } from './auth/guards/auth.guard';
+// import { AuthGuard } from './auth/guards/auth.guard';
 import { PublicGuard } from './auth/guards/public.guard';
 
 const routes: Routes = [
@@ -14,8 +15,8 @@ const routes: Routes = [
   {
     path: 'heroes',
     loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule ),
-    canActivate: [ AuthGuard ],
-    canMatch: [ AuthGuard ]
+    canActivate: [ authGuard ],
+    canMatch: [ authGuard ]
   },
   {
     path: '404',
--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,39 +1,61 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from "@angular/router";
-import { Observable, tap } from "rxjs";
-import { AuthService } from "../services/auth.service";
+import { Injectable, inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateFn,
+  CanMatch,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-@Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanMatch, CanActivate{
-    
-    constructor( 
-        private authService:AuthService,
-        private router: Router
-    ){}
-    private checkAuthStatus(): boolean | Observable<boolean>{
-        return this.authService.checkAuthentication()
-        .pipe(
-            tap( isAuthenticated => console.log('Authenticated: ', isAuthenticated) ),
-            tap( isAuthenticated => {
-                if(!isAuthenticated){
-                    this.router.navigate(['./auth/login'])
-                }
-            }),
-            
-        );
-    }
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.checkAuthentication().pipe(
+    tap((isAuthenticated) => console.log('Authenticated: ', isAuthenticated)),
+    tap((isAuthenticated) => {
+      if (!isAuthenticated) {
+        router.navigate(['./auth/login']);
+      }
+    })
+  );
+};
+// @Injectable({ providedIn: 'root' })
+// export class AuthGuard implements CanMatch, CanActivate{
 
-    canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean>  {
-        // console.log('Can Match');
-        // console.log({ route, segments });
-        
-        return this.checkAuthStatus();
-    }
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
-        // console.log('Can activated');
-        // console.log({ route, state });
-        
-        return this.checkAuthStatus();
-    }
-    
-}
+//     constructor(
+//         private authService:AuthService,
+//         private router: Router
+//     ){}
+//     private checkAuthStatus(): boolean | Observable<boolean>{
+//         return this.authService.checkAuthentication()
+//         .pipe(
+//             tap( isAuthenticated => console.log('Authenticated: ', isAuthenticated) ),
+//             tap( isAuthenticated => {
+//                 if(!isAuthenticated){
+//                     this.router.navigate(['./auth/login'])
+//                 }
+//             }),
+
+//         );
+//     }
+
+//     canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean>  {
+//         // console.log('Can Match');
+//         // console.log({ route, segments });
+
+//         return this.checkAuthStatus();
+//     }
+//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+//         // console.log('Can activated');
+//         // console.log({ route, state });
+
+//         return this.checkAuthStatus();
+//     }
+
+// }
