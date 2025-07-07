5437041a96d3f8db60d201e87b1d55fc76b24e02
uriel.martinez <uriel.martinez@inateltech.com>
2025-07-07 03:04:01 -0600

    se añaden cambios de enviroments, mensaje alert y script deploy

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,4 +1,4 @@
 
 export const environments = {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'https://json-mock-api-heroes.onrender.com'
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,4 +2,5 @@
 
 export const environments = {
     baseUrl: 'http://localhost:3000'
+    // baseUrl: 'https://json-mock-api-heroes.onrender.com'
 }
